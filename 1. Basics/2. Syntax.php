3c9d0e7173b30ee902a4f886d9d70c1705ed6d23
dvir-karilker <86989184+dvir-karilker@users.noreply.github.com>
2024-01-21 12:26:54 +0200

    Update 2. Syntax.php

--- 1. Basics/2. Syntax.php
+++ 1. Basics/2. Syntax.php
@@ -1,8 +1,15 @@
-<?php 
+<?php
 
 // PHP can be written Before, Inside and After your HTML Code.
-/* PHP Scripts starts with <?php and ends with ?> : */
-/* 
+
+// PHP Comments are written using:
+//  "//" - For a Single Line
+/* "/*" - For
+          Multi
+          Line
+          Comments */
+
+/* PHP Scripts starts with <?php and ends with ?> : 
 <?php
 // PHP code goes here
 ?>
