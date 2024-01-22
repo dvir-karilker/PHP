f2546ab1d61bad9e6ad50d6b731c50125a4ee55b
dvir-karilker <86989184+dvir-karilker@users.noreply.github.com>
2024-01-22 12:06:57 +0200

    Update 2. Syntax.php

--- 1. Basics/2. Syntax.php
+++ 1. Basics/2. Syntax.php
@@ -68,7 +68,7 @@ echo "Hello World!";
 EcHo "Hello World!";
 
 // BUT! Variables are case-sensitive!
-// For example, $color is not the same as $COLOR (we will learn about Variables in unit 3 under "1. Basics/3. Variables").
+// For example, $color is not the same as $COLOR (we will learn about Variables in unit 3 under "1. Basics/3. Variables & Data Types").
 
 
 ?>
